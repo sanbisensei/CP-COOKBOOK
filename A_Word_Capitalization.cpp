c1f141e759bdd3debc84d1acb3b752459e5de5ef
sanbisensei <lsunbi1@gmail.com>
2026-02-15 23:54:00 +0600

    8/10

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    
+    for(int i=0;i<s.size();i++){
+        if(i==0){
+            cout<<char(toupper(s[i]));
+        }
+        else{
+            cout<<s[i];
+        }
+        
+        
+    }
+    return 0;
+}
