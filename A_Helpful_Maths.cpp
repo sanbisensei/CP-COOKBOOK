7e1e8861141f8a715523acd47fc07f3e9ed52776
sanbisensei <lsunbi1@gmail.com>
2026-02-15 23:40:51 +0600

    7/10

--- A_Helpful_Maths.cpp
+++ A_Helpful_Maths.cpp
@@ -5,26 +5,38 @@ int main(){
     string s;
     cin>>s;
     int cone=0;int ctwo=0;int cthree=0;
+
     for(char i: s){
-        if(i==1){
+        if(i=='1'){
             cone++;
         }
-        else if(i==2){
+        else if(i=='2'){
             ctwo++;
         }
-        else if(i==3){
+        else if(i=='3'){
             cthree++;
         }
     }
-    for(int i=0;i<cone;i++){
-        cout<<"1"<<"+";
-    }
-    for(int i=0;i<ctwo;i++){
-        cout<<"2"<<"+";
-    }
-    for(int i=0;i<cthree;i++){
-        cout<<"3"<<"+";
-    }
+   bool first = true;
+
+        for(int i=0;i<cone;i++){
+            if(!first) cout<<"+";
+            cout<<"1";
+            first = false;
+            }
+        for(int i=0;i<ctwo;i++){
+            if(!first) cout<<"+";
+            cout<<"2";
+            first = false;
+            }
+        for(int i=0;i<cthree;i++){
+            if(!first) cout<<"+";
+            cout<<"3";
+            first = false;
+            }
+
 
     return 0;
 }
+
+
