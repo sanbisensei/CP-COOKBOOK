edac1948515630dab011bdecc3f5a34f9f82d9cb
sanbisensei <lsunbi1@gmail.com>
2026-02-20 15:19:09 +0600

    2/30

--- A_Young_Physicist.cpp
+++ A_Young_Physicist.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+     int n;cin>>n;
+     int x,y,z;
+     int sumx=0,sumy=0,sumz=0;
+     for(int i=0;i<n;i++){
+        cin>>x>>y>>z;
+        sumx+=x;
+        sumy+=y;
+        sumz+=z;
+     }
+
+     if(sumx == 0 && sumy == 0 && sumz == 0){
+        cout<<"YES"<<endl;
+     } 
+     else{
+        cout<<"NO"<<endl;
+     }
+     
      
     return 0;
 }
