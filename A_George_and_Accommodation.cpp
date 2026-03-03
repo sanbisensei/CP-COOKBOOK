e27aa41f94bbb218f7aa452fa5d3e754de1bef82
sanbisensei <lsunbi1@gmail.com>
2026-03-04 02:26:36 +0600

    Add A. George and Accommodation problem solution

---
+++ A_George_and_Accommodation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    int count=0;
+    while(t--){
+        int p,q;cin>>p>>q;
+    
+    if(p<=q-2 && q>=2){
+        count++;
+    }
+    }
+    cout<<count<<endl;
+    return 0;
+}
