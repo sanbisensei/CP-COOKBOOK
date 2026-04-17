dc3e878712a1b203ba4a6f4809425f97a08d1c77
sanbisensei <lsunbi1@gmail.com>
2026-04-17 18:45:37 +0600

    failed to attempt 1300 rated problem

---
+++ A_Cut_Ribbon.cpp
@@ -0,0 +1,19 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// //mail_man will rise
+// using ll = long long;
+// constexpr ll mod = 1e9+7;
+
+// int main(){
+//     ll n;cin>>n;
+//     ll a[n];
+//     vector<ll> v;
+//     for(ll i=0;i<n;i++) {
+//     cin>>a[i];
+//     v.push_back(a[i]);
+//     }
+//     sort(v.begin(),v.end());
+//     cout << v[0] << endl;
+
+//     return 0;
+// }
