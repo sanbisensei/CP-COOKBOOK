43ee050749b7d3d60e8b402b4c9825b126d6210b
sanbisensei <lsunbi1@gmail.com>
2026-05-21 01:15:59 +0600

    added the editorial for the last solution

--- C_The_67_th_Permutation_Problem.cpp
+++ C_The_67_th_Permutation_Problem.cpp
@@ -33,8 +33,19 @@ int main(){
     return 0;
 }
 
-
-
-
-
-// 
+/**
+ * {
+            if(i%3==0 or i%3==1){
+                ans.push_back(b--);
+            }
+            else{
+                ans.push_back(s++);
+            }
+        }
+            > ei pattert always amake [BIG BIG SMALL] output dibe
+            > ar amader ei pattern i lagto coz amader 3ta digit er 
+            middle value=media ber korte hobe jeita shobche boro value o
+            hobe na abar shobche choto value o hobe na. 
+            > so amader ei [BIG BIG SMALL] pattern i lagbe.
+            
+ */
