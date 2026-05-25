59dad37649ce9ec783160b5759b4b27291e28911
sanbisensei <lsunbi1@gmail.com>
2026-05-26 02:32:52 +0600

    add solution for "B. Right Maximum" from Codeforces Educational Round 188 (Div. 2)

--- B_Right_Maximum.cpp
+++ B_Right_Maximum.cpp
@@ -6,5 +6,25 @@ constexpr ll mod = 1e9+7;
 
 int main(){
     
+        int t;
+        cin >> t;
+        while(t--){
+            int n;cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int mx=0;
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>=mx){
+            c++;
+            mx=max(arr[i],mx);
+        }
+
+    }
+    cout<<c<<endl;
+    
+        }
     return 0;
 }
