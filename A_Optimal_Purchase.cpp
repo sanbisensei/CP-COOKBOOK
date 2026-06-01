5d8124e7e74eafab15debc083b5e72d2a0b059de
sanbisensei <lsunbi1@gmail.com>
2026-06-01 18:41:44 +0600

    A. Optimal Purchase solution (cf edu round)

--- A_Optimal_Purchase.cpp
+++ A_Optimal_Purchase.cpp
@@ -5,32 +5,15 @@ using ll = long long;
 constexpr ll mod = 1e9+7;
 
 int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        ll n,a,b;cin>>n>>a>>b;
+    
+        int t;
+        cin >> t;
+        while(t--){
+            ll n,a,b;cin>>n>>a>>b;
         ll f = n/3;
-        ll s = n-(f*3);
-        if(a<b && n>=3){
-            if(a*3<b*1){
-            cout<<(n*a)<<endl;
+        ll s = n%3;
+        cout<<min({n*a,a*s+b*f,(f+1)*b})<<endl;
         }
-        else{
-            cout<<(s*a)+(f*b)<<endl;
-        }  
-        }
-        else if(n<=2){
-            if(a*n < b){
-                cout<<(a*n)<<endl;
-            }
-            else{
-                cout<<b<<endl;
-            }
-            
-        }
-        else{
-            cout<<(s*b)+(f*a)<<endl;
-        }
-    }
+        
     return 0;
 }
