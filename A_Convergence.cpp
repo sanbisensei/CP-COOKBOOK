9f637d0a4b06d7b8d6ed0dcd26a887607b110733
sanbisensei <lsunbi1@gmail.com>
2026-06-06 02:29:48 +0600

    done

--- A_Convergence.cpp
+++ A_Convergence.cpp
@@ -10,33 +10,16 @@ int main(){
     while(t--){
         int n;cin>>n;
     vector<int> arr(n);
-    map<int,int> m;
-    set<int> s;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        s.insert(arr[i]);
-        m[arr[i]]++;     
-    }
-    int hablu=0;
-    if(s.size()!=n){
-        hablu = 1;
-        int maxf = 0;
-    int target =0;
-    for(auto i:m){
-        if(i.second>maxf){
-            maxf = i.second;
-            target = i.first;
+    int c=0;
+    
+    for(int i=0;i<n;i++) cin>>arr[i];
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n/2;i++){
+        if(arr[i]!=arr[n-1-i]){
+            c++;
         }
     }
-    arr.erase(remove(arr.begin(),arr.end(),target),arr.end());
-    }
-    int size = arr.size();
-    if(size==1){
-        cout<<1<<endl;
-    }
-    else{
-        cout<<ceil(double(size/2))+hablu<<endl;
-    }
+    cout<<c<<endl;
     }
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -5,34 +5,7 @@ using ll = long long;
 constexpr ll mod = 1e9+7;
 
 int main(){
-    int n;cin>>n;
-    vector<int> arr(n);
-    map<int,int> m;
-    set<int> s;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        s.insert(arr[i]);
-        m[arr[i]]++;     
-    }
-    int hablu=0;
-    if(s.size()!=n){
-        hablu = 1;
-        int maxf = 0;
-    int target =0;
-    for(auto i:m){
-        if(i.second>maxf){
-            maxf = i.second;
-            target = i.first;
-        }
-    }
-    arr.erase(remove(arr.begin(),arr.end(),target),arr.end());
-    }
-    int size = arr.size();
-    if(size==1){
-        cout<<1<<endl;
-    }
-    else{
-        cout<<ceil(double(size/2))+hablu<<endl;
-    }
+    cout<<3/2<<endl;
+    
     return 0;
 }
