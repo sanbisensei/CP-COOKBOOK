af653ab2a019016c7aaae1c688d57137e77bcd9b
sanbisensei <lsunbi1@gmail.com>
2026-06-07 01:11:50 +0600

    Refactor solution for "A. Marisa Steals Reimu's Takeout" problem to improve clarity and efficiency

--- A_Marisa_Steals_Reimu_s_Takeout.cpp
+++ A_Marisa_Steals_Reimu_s_Takeout.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 //mail_man will rise
+// learned from imrancp496's solution
 using ll = long long;
 constexpr ll mod = 1e9+7;
 
@@ -10,23 +11,21 @@ int main(){
     while(t--){
         int n;cin>>n;
     vector<int> arr(n);
-    int c=0;
+    int czero=0;
     int cone=0;
     int ctwo=0;
     for(int i=0;i<n;i++){
         cin>>arr[i];
-        if(arr[i]%3==0){
-            c++;
-        }
-        if(arr[i]%3==1){
-            cone++;
-        }
-        if(arr[i]%3==2){
-            ctwo++;
-        }
+        if(arr[i]==0) czero++;
+        if(arr[i]==1) cone++;
+        if(arr[i]==2) ctwo++;
     }
-    int halua = (cone+ctwo)/2;
-    cout<< c + halua <<endl;
+    int ans = czero + min(cone,ctwo);
+    int mn = min(cone,ctwo);
+    cone -= mn;
+    ctwo -= mn;
+    cout<< ans+ cone/3 + ctwo/3 <<endl;
+
 }
     return 0;
 }
