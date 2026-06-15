2bda4b6391c940f679129976dbc003b13edf5dd4
sanbisensei <lsunbi1@gmail.com>
2026-06-15 14:42:29 +0600

    3

---
+++ A_Construct_an_Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;cin>>n;
+    for(int i=0;i<n;i++){
+        cout<< (n*2)-i <<" ";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
-//mail_man will rise
-using ll = long long;
-constexpr ll mod = 1e9+7;
-
-int main(){
-    int n,x,y,z;
-    cin>> n>>x>>y>>z;
-    
-    
-    return 0;
-}
+n
