95407808f9bb55caecb2208c6592b6a8d2bf5150
sanbisensei <lsunbi1@gmail.com>
2026-08-31 14:26:14 +0600

    Gigantomachy solved

--- B_Gigantomachy.cpp
+++ B_Gigantomachy.cpp
@@ -5,10 +5,11 @@ using ll = long long;
 constexpr ll mod = 1e9+7;
 
 int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n,m;cin>>n>>m;
+   
+int t;
+cin >> t;
+while(t--){
+            int n,m;cin>>n>>m;
     vector<int> arrn(n);
     vector<int> arrm(m);
     for(int i=0;i<n;i++){
@@ -17,31 +18,14 @@ int main(){
     for(int i=0;i<m;i++){
         cin>>arrm[i];
     }
-
-    int mxn= *max_element(arrn.begin(), arrn.end());
-    int mxm= *max_element(arrm.begin(), arrm.end());
-    int mnm= *min_element(arrm.begin(),arrm.end());
-
-    int sumn = accumulate(arrn.begin(), arrn.end(), 0);
-    int summ = accumulate(arrm.begin(), arrm.end(), 0);
-
-    if(n == 1 && m == 1){
-        if(arrn[0]>=arrm[0]){
-            cout<< 1<<endl;
-        }
-        else{
-            cout<<2<<endl;
-        }
+    if((arrn[0]+n-1)>=arrm[0]+m-1){
+        cout<<1<<endl;
     }
     else{
-        if(summ>sumn && (mxm-mnm>1)){
-            cout<< 2<<endl;
-        }
-        else{
-            cout<< 1<<endl;
-        }
+        cout<<2<<endl;
     }
-    }
+}
+
 
     return 0;
 }
