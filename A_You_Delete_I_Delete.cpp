8e5d52f790ce70572ebe459050175afcf960e6c4
sanbisensei <lsunbi1@gmail.com>
2026-09-01 18:46:15 +0600

    2/10

--- A_You_Delete_I_Delete.cpp
+++ A_You_Delete_I_Delete.cpp
@@ -8,23 +8,25 @@ int main(){
     int t;
     cin >> t;
     while(t--){
+        
         string s;cin>>s;
-        bool skip = false;
-    for(int i=0;i<s.size()-1;i++){
-        if(s[i]=='1' && skip==false){
-            skip= true;
-            continue;
+        
+    for(int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+                s.erase(i,1);
+                break;
+            }
         }
-        if(s[i]=='0' && s[i+1]=='1'){
-            s.erase(i,2);
-            break;
-        }
-        if(s[i]=='1'&& s[i+1]=='0'){
-            s.erase(i,2);
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='1'){
+            s.erase(i,1);
             break;
         }
     }
-    cout<<s<<endl;
-    }
+     for(int i=0;i<s.size();i++){
+            cout<<s[i];
+        }
+        cout<<endl;
+    }  
     return 0;
 }
