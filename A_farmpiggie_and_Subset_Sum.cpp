41621552f364b454b3d75c0e262238364e58e6f1
sanbisensei <lsunbi1@gmail.com>
2026-09-03 00:45:22 +0600

    frampiggie DONE

---
+++ A_farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+    cin>>n;
+    for (int i = n;i>0; i--)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    
+    }
+    return 0;
+}
