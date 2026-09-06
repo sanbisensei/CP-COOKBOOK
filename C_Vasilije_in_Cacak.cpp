f3a43bef2f6746938e6035fa57f66ef4bee6a9e8
sanbisensei <lsunbi1@gmail.com>
2026-09-07 00:40:12 +0600

    another 900

---
+++ C_Vasilije_in_Cacak.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll n,k,x;cin>>n>>k>>x;
+    
+        ll min_sum = (k*(k+1))/2;
+        ll max_sum = k*(n+(n-k+1))/2;
+    if(x>=min_sum && x<=max_sum){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+    }
+    return 0;
+}
