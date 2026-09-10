925883e8c0fe9137d70eb09aae48ff7fc0cea940
sanbisensei <lsunbi1@gmail.com>
2026-09-10 20:07:06 +0600

    No More TLE

--- B_1_Carrot_Chopdown_Easy_Version.cpp
+++ B_1_Carrot_Chopdown_Easy_Version.cpp
@@ -3,44 +3,41 @@ using namespace std;
 //mail_man will rise
 using ll = long long;
 constexpr ll mod = 1e9+7;
- 
+
 int main(){
-  int t;
-  cin >> t;
-  while(t--){
-        int n,m;
+ int t;
+ cin >> t;
+ while(t--){
+      
+    int n,m;
     cin>>n>>m;
     vector<int> v(n);
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
     
-    
-    int maxFreq = 0;
-    vector<int> freqs;
-    vector<int> chopped;
-    for(int j=1;j<=m;j++){
-        chopped.clear();
-        for(int i=0;i<n;i++){
-        if(v[i]<=j){
-            chopped.push_back(v[i]);
+    vector<int> freq(m+1);
+    for(int i=0;i<n;i++){
+        freq[v[i]]++;
+    }
+
+    vector<int> suffix(m+2);
+    for(int i=m;i>=1;i--){
+        suffix[i] = suffix[i+1]+freq[i];
+    }
+
+    int ans =0;
+
+    for(int x=0;x<=m;x++){
+        int now = suffix[x];
+
+        if(2*x <=m){
+            now+=freq[2*x];
         }
-        else if(v[i]>j){
-            int s = v[i]-j;
-            chopped.push_back(s);
-            chopped.push_back(j);
-        }  
-        
+        ans = max(ans,now);
     }
-    int freq = count(chopped.begin(), chopped.end(), j);
-    maxFreq = max(maxFreq, freq); 
-    freqs.push_back(maxFreq);
-    }
+    cout<<ans<<endl;
     
-    int mx = *max_element(freqs.begin(), freqs.end());
-    cout<<mx<<endl; 
-    
- 
-  }
+ }
     return 0;
 }
