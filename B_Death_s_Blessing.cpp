1d2645f6bd52520f0bddeaa43f034c05c657adfc
sanbisensei <lsunbi1@gmail.com>
2026-09-11 21:12:58 +0600

    Deaths Blessings

--- B_Death_s_Blessing.cpp
+++ B_Death_s_Blessing.cpp
@@ -5,41 +5,24 @@ using ll = long long;
 constexpr ll mod = 1e9+7;
 
 int main(){
- int t;
- cin >> t;
- while(t--){
+int t;
+cin >> t;
+while(t--){
+    
         int n;
     cin>>n;
     vector<ll> a(n);
-    vector<pair<ll,ll>> b;
+    vector<ll> b(n);
     for(int i=0;i<n;i++){
         cin>> a[i];
     }
     for(int i=0;i<n;i++){
-        int x;cin>>x;
-        b.push_back({x,i});
+        cin>>b[i];
     }
     sort(b.begin(),b.end());
-    ll time=0;
-    for(int i=0;i<n;i++){
-        int idx = b[i].second;
-        time = time + a[idx];
-        if(idx==0){      
-            if(n>1){
-                a[idx+1] = a[idx+1]+ b[i].first;
-            }
-        }
-        else if(idx==(n-1)){
-            a[idx-1] = a[idx-1]+b[i].first;
-        }
-        else{
-            a[idx+1] = a[idx+1]+ b[i].first;
-            a[idx-1] = a[idx-1]+b[i].first;
-
-        }
-    }
-
-    cout<<time<<endl;
- }
+    
+    cout<<accumulate(a.begin(),a.end(), 0LL)+accumulate(b.begin(),b.end(), 0LL)-b[n-1]<<endl;
+ 
+}
     return 0;
 }
