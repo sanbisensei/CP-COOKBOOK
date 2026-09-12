90afdfe7fa7cfbedb1687d48998cb7628f4f8018
sanbisensei <lsunbi1@gmail.com>
2026-09-13 01:45:19 +0600

    Refactor code structure for improved readability and maintainability

---
+++ B_Min_Matrices.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;cin>>n>>k;
+
+    if(n>k){
+        cout<<"-1"<<endl;
+        continue;
+    }
+    vector<vector<int>> v(n, vector<int>(n));
+    int x=1;
+    for(int i=0;i<n;i++){
+        
+        for(int j=0;j<n;j++){
+            v[i][j]=x;
+            x++;
+        }
+        
+    }
+    
+    for(int i=0;i<n;i++){
+        swap(v[0][i],v[i][i]);
+        
+    }
+
+    for(int i=0;i<n;i++){
+        
+        for(int j=0;j<n;j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    }
+    return 0;
+}
