0afa913f119b3c2bb13cd3513132c90587372aea
sanbisensei <lsunbi1@gmail.com>
2026-09-12 14:30:43 +0600

    1

---
+++ A_Gravity_Flip.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for(int x:v){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ B_Distinct_Elements.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+//mail_man will rise
+using ll = long long;
+constexpr ll mod = 1e9+7;
+
+int main(){
+    
+    return 0;
+}
