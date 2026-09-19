926a01bdf335b7282fc51e95c2983b1c0434e59b
sanbisensei <lsunbi1@gmail.com>
2026-09-19 20:28:18 +0600

    refactor: remove unused B_Monocarp_and_Projects files and clean up code

--- UPSOLVE EDU 194/B_Monocarp_and_Projects.cpp
+++ UPSOLVE EDU 194/B_Monocarp_and_Projects.cpp
@@ -3,26 +3,25 @@ using namespace std;
 //mail_man will rise
 using ll = long long;
 constexpr ll mod = 1e9+7;
-// mathar upor diye gese
-// brute force easy but math diye kora hard
+
 int main(){
     int t;
     cin >> t;
     while(t--){
-        ll x,y,k, ans=0;
-    cin>>x>>y>>k;
-    
-    ll z = y -x;
-    ll last = x+k-1;
-    int c = min(last,z); // eita check kortisi j last value z er che boro kina
-
-    for(int i=x;i<=c;i++){
-        ans = ans +(z%i);
+        ll x;cin>>x;
+    ll y;cin>>y;
+    ll k;cin>>k;
+    ll ans=0;
+    ll ck=k;
+    for(ll i=0;i<k;i++){
+        if((y-x)<(x+i)){
+            ans += (k-i) * (y-x);
+            break;
+        }
+        ans = ans+ ((y-x)%(x+i));
+        
     }
-    if(last > z){
-        ans+= (last - max(x,z+1)+1)*z;
-    }
-     cout<< ans<< endl;
+    cout<<ans<<endl;
     }
     return 0;
 }
